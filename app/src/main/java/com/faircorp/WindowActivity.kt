ba615bb905c78e945c0eb65a45a432b85648fd22
bouthaina-hrour <73646814+bouthaina-hrour@users.noreply.github.com>
2021-11-11 19:16:16 +0100

    update windowActivity

--- app/src/main/java/com/faircorp/WindowActivity.kt
+++ app/src/main/java/com/faircorp/WindowActivity.kt
@@ -1,7 +1,13 @@
 package com.faircorp
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import com.faircorp.model.ApiServices
 import com.faircorp.service.WindowService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class WindowActivity : BasicActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,14 +17,45 @@ class WindowActivity : BasicActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val windowService =WindowService()
         val id = intent.getLongExtra(WINDOW_NAME_PARAM, 0)
-        val window = windowService.findById(id)
-        if (window != null) {
-            findViewById<TextView>(R.id.window_name).text = window.name
-            findViewById<TextView>(R.id.room_name).text = window.roomName
-            //findViewById<TextView>(R.id.window_current_temperature).text = window.room.currentTemperature?.toString()
-           // findViewById<TextView>(R.id.window_target_temperature).text = window.room.targetTemperature?.toString()
-            findViewById<TextView>(R.id.window_status).text = window.windowStatus.toString()
+
+
+        var windowName : String
+        var windowStatus : String
+        var roomName : String
+        var currentTemperature : String?
+
+        lifecycleScope.launch(context = Dispatchers.IO) { // (1)
+            runCatching { ApiServices().windowsApiService.findById(id).execute() } // (2)
+                .onSuccess {
+                    withContext(context = Dispatchers.Main) { // (3)
+                        val window = it.body()
+                        if (window != null) {
+                            windowName = window.name
+                            roomName = window.roomName
+                            windowStatus = window.windowStatus.toString()
+                            findViewById<TextView>(R.id.txt_window_name_item).text = windowName
+                            findViewById<TextView>(R.id.txt_window_room).text = roomName
+                            findViewById<TextView>(R.id.txt_status).text = windowStatus
+
+                            }
+
+                        }
+
+                    }
+
+
+                .onFailure {
+                    withContext(context = Dispatchers.Main) { // (3)
+                        Toast.makeText(
+                            applicationContext,
+                            "Error on window loading $it",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+
         }
+
 
     }
 }
